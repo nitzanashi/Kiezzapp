0ccffdd4e2a07d22b8cae5eaae0785e2345d3571
Nitzan Nashi <n.nashi@campus.tu-berlin.de>
2018-12-18 13:46:18 +0000

    HA8 - Adding Phone mode

---
+++ functions.js
@@ -0,0 +1,8 @@
+			function clickMe() {
+			if (document.getElementById('navi').style.height == "100%"){
+			    document.getElementById('navi').style.height = "0%";
+			} else {
+			    document.getElementById('navi').style.height = "100%";
+
+			  }
+			}

958d1784f8e836e5bba0ac112ebbdb6ef4145d84
Nitzan Nashi <n.nashi@campus.tu-berlin.de>
2018-12-19 07:53:47 +0000

    HA8 update

--- functions.js
+++ functions.js
@@ -1,8 +1,12 @@
 			function clickMe() {
 			if (document.getElementById('navi').style.height == "100%"){
 			    document.getElementById('navi').style.height = "0%";
+			    document.getElementById('navi_ul').style.display = "none";
+
 			} else {
 			    document.getElementById('navi').style.height = "100%";
+			    document.getElementById('navi_ul').style.display = "block";
+
 
 			  }
 			}
